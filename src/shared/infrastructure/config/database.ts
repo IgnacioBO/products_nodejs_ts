44a36ea2175fc8c82198f1aa626523abdea2256f
Ignacio Barria <ignacio.barria.o@gmail.com>
2025-11-14 19:45:15 -0300

    change connectionString to work with gpc

--- src/shared/infrastructure/config/database.ts
+++ src/shared/infrastructure/config/database.ts
@@ -11,15 +11,29 @@ import { Pool } from 'pg';
 // osea DB_USER = process.env.DB_USER
 const { DB_PRODUCT_USER, DB_PRODUCT_PASS, DB_PRODUCT_HOST, DB_PRODUCT_PORT, DB_PRODUCT_NAME } = process.env;
 
+if (!DB_PRODUCT_USER || !DB_PRODUCT_PASS || !DB_PRODUCT_HOST || !DB_PRODUCT_NAME) {
+  throw new Error('Faltan envs de Postgres');
+}
+
+//hacemo encodeURIComponent para codificar caracteres especiales en la URI, como @, :, /, que podria tener una contraseña y no tener errores.
+const user = encodeURIComponent(DB_PRODUCT_USER);
+const pass = encodeURIComponent(DB_PRODUCT_PASS);
+const db   = encodeURIComponent(DB_PRODUCT_NAME);
+const socket = encodeURIComponent(DB_PRODUCT_HOST);
+
 //La clase Pool es un grupo de conexiones a la base de datos
 //Esto permite crear un grupo de conexiones a la base de datos y reutilizarlas
 //Esto es más eficiente que crear una nueva conexión cada vez que se necesita acceder a la base de datos
+
 const pool = new Pool({
-  connectionString: `postgres://${DB_PRODUCT_USER}:${DB_PRODUCT_PASS}@${DB_PRODUCT_HOST}:${DB_PRODUCT_PORT}/${DB_PRODUCT_NAME}`,
+  //connectionString: `postgres://${user}:${pass}@${socket}:${DB_PRODUCT_PORT}/${db}`,
+  // Usamos socket en lugar de host para conectar via socket unix en GCP 
+  //ademas el formato tendra @locahost y el host ira en ?host= parte de query string, esto es necesario para que funcione la conexion via socket en GCP
+  connectionString: `postgresql://${user}:${pass}@localhost:${DB_PRODUCT_PORT}/${db}?host=${socket}`,
 });
 
 export async function verifyPostgresConnection(timeoutMs = 8000) {
-  console.log("Connection string:", `postgres://${DB_PRODUCT_USER}:${DB_PRODUCT_PASS}@${DB_PRODUCT_HOST}:${DB_PRODUCT_PORT}/${DB_PRODUCT_NAME}`);
+  console.log("Connection string:", `postgresql://${user}:${pass}@localhost:${DB_PRODUCT_PORT}/${db}?host=${socket}`);
   const q = pool.query('select now() as now');
   const t = new Promise((_, rej) => setTimeout(() => rej(new Error('pg connect timeout')), timeoutMs));
   console.log("✅ Conexión a PostgreSQL establecida");
